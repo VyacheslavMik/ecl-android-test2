bb9c9a82d99f90b7108dabf5fb6cff3422637b51
kosbar <kosbar28@gmail.com>
2024-09-11 00:17:17 +0500

    Update libs for ecl

--- app/src/main/assets/lisp/etc/init.lisp
+++ app/src/main/assets/lisp/etc/init.lisp
@@ -4,11 +4,18 @@
 	(ext:lisp-implementation-vcs-id))
 
 (defvar *ecl-home* *default-pathname-defaults*)
+(format t "LISP DIRECTORY: ~A" *ecl-home*)
+(format t "LISP DIRECTORY2 : ~A" *default-pathname-defaults*)
 
+(format t "ZDEC0")
 (format t "Loading the modules~%")
-(require '#:asdf)
 (require '#:sockets)
+
+(format t "ZDEC2")
+(require '#:asdf)
+(format t "ZDEC3")
 (require '#:serve-event)
+(format t "ZDEC4")
 
 (setf asdf:*user-cache* (merge-pathnames #P"../cache/" *default-pathname-defaults*))
 
